201702e27172960cada3714bfa1d7a8d52808c7f
Armando1509 <armandogomez1509@gmail.com>
2024-06-11 09:37:56 -0600

    creacion de alerta y state

--- View/DetallesCliente.js
+++ View/DetallesCliente.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Alert } from "react-native";
+import { Headline, Text, Subheading, Button, FAB } from "react-native-paper";
+import globalStyles from "../styles/global";
+import axios from "axios";
 
-const DetallesCliente = () => {
+const DetallesCliente = ({navigation, route}) => {
+  const {guardarConsultarAPI} = route.params
+  const {nombre, correo, telefono, empresa, id} = route.params.item
+  const mostrarConfirmacion = () =>{
+    Alert.alert(
+      '¿Desea eliminar este cliente?',
+      'Un contacto eliminado no se puede recuperar',
+      [
+        {text: 'Si, Eliminar', onPress: () => eliminarContacto() },
+        {text: 'Cancelar', style: 'cancel'}
+      ]
+    )
+  }
+  
   return (
     <View>
       <Text>Desde detalles cliente</Text>
